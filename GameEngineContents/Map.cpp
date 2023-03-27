b2c17611f8be5b2a3b06b33e809db0f416d58857
yeyoungK <clavisfloccu@naver.com>
2023-03-27 17:40:40 +0900

    김예영_feat(MapModifier, Map): 맵 오브젝트 랜덤 생성 및 사각형으로 땅깎는 함수 업데이트

--- GameEngineContents/Map.cpp
+++ GameEngineContents/Map.cpp
@@ -25,87 +25,87 @@ Map::~Map()
 
 void Map::Start()
 {
-	MainMap = this;
+    MainMap = this;
 
-	if (nullptr == MainMap)
-	{
-		MsgAssert("MainMap이 nullptr입니다.");
-		return;
-	}
-
-
-	Maps.push_back("MapCity.bmp");
-	Maps.push_back("MapBooks.bmp");
-	Maps.push_back("MapCars.bmp");
-	Maps.push_back("MapTrain.bmp");
-
-	ColMaps.push_back("MapCity_Ground.bmp");
-	ColMaps.push_back("MapBooks_Ground.bmp");
-	ColMaps.push_back("MapCars_Ground.bmp");
-	ColMaps.push_back("MapTrain_Ground.bmp");
-
-	MapName = Maps[MapMode];
-	ColMapName = ColMaps[MapMode];
-
-	//입력 키 생성
-	if (false == GameEngineInput::IsKey("FreeMoveSwitch"))
-	{
-		GameEngineInput::CreateKey("FreeMoveSwitch", '0');
-		GameEngineInput::CreateKey("FreeLeft", VK_LEFT);
-		GameEngineInput::CreateKey("FreeRight", VK_RIGHT);
-		GameEngineInput::CreateKey("FreeUp", VK_UP);
-		GameEngineInput::CreateKey("FreeDown", VK_DOWN);
-
-		//디버깅 모드_충돌맵 확인
-		GameEngineInput::CreateKey("DebugMode", '9');
-	}
-
-	//마우스 좌측 키 입력 생성
-	if (false == GameEngineInput::IsKey("LandHole"))
-	{
-		GameEngineInput::CreateKey("LandHole", VK_LBUTTON);
-	}
+    if (nullptr == MainMap)
+    {
+        MsgAssert("MainMap이 nullptr입니다.");
+        return;
+    }
 
 
-	//MapRender 생성
-	MapRender = CreateRender(WormsRenderOrder::Map);
-	MapRender->SetImage(MapName);
-	float4 MapScale = MapRender->GetImage()->GetImageScale();
-	MapRender->SetPosition(MapScale.half());
-	MapRender->SetScaleToImage();
+    Maps.push_back("MapCity.bmp");
+    Maps.push_back("MapBooks.bmp");
+    Maps.push_back("MapCars.bmp");
+    Maps.push_back("MapTrain.bmp");
 
-	//ColMapRender 생성
-	{
-		ColMapRender = CreateRender(WormsRenderOrder::Map);
-		ColMapRender->SetImage(ColMapName);
-		ColMapRender->SetPosition(MapScale.half());
-		ColMapRender->SetScaleToImage();
+    ColMaps.push_back("MapCity_Ground.bmp");
+    ColMaps.push_back("MapBooks_Ground.bmp");
+    ColMaps.push_back("MapCars_Ground.bmp");
+    ColMaps.push_back("MapTrain_Ground.bmp");
+
+    MapName = Maps[MapMode];
+    ColMapName = ColMaps[MapMode];
+
+    //입력 키 생성
+    if (false == GameEngineInput::IsKey("FreeMoveSwitch"))
+    {
+        GameEngineInput::CreateKey("FreeMoveSwitch", '0');
+        GameEngineInput::CreateKey("FreeLeft", VK_LEFT);
+        GameEngineInput::CreateKey("FreeRight", VK_RIGHT);
+        GameEngineInput::CreateKey("FreeUp", VK_UP);
+        GameEngineInput::CreateKey("FreeDown", VK_DOWN);
+
+        //디버깅 모드_충돌맵 확인
+        GameEngineInput::CreateKey("DebugMode", '9');
+    }
+
+    //마우스 좌측 키 입력 생성
+    if (false == GameEngineInput::IsKey("LandHole"))
+    {
+        GameEngineInput::CreateKey("LandHole", VK_LBUTTON);
+    }
+
+
+    //MapRender 생성
+    MapRender = CreateRender(WormsRenderOrder::Map);
+    MapRender->SetImage(MapName);
+    float4 MapScale = MapRender->GetImage()->GetImageScale();
+    MapRender->SetPosition(MapScale.half());
+    MapRender->SetScaleToImage();
+
+    //ColMapRender 생성
+    {
+        ColMapRender = CreateRender(WormsRenderOrder::Map);
+        ColMapRender->SetImage(ColMapName);
+        ColMapRender->SetPosition(MapScale.half());
+        ColMapRender->SetScaleToImage();
 
         ColMapRender->Off();
-	}
+    }
 
-	//BackGround_Sky
-	{
-		GameEngineRender* BackGround = CreateRender(WormsRenderOrder::BackGround);
-		BackGround->SetImage("gradient.bmp");
-		BackGround->SetPosition(MapScale.half());
-		BackGround->SetScaleToImage();
-	}
-	//BackGround_Mountain
-	{
-		GameEngineRender* BackGround = CreateRender(WormsRenderOrder::BackGround);
-		BackGround->SetImage("Midground.bmp");
-		BackGround->SetPosition(MountainPos);
-		BackGround->SetScale(BackScale);
-	}
-	//BackGround_Wave
-	{
-		GameEngineRender* WaveBack = CreateRender(WormsRenderOrder::BackGround);
-		WaveBack->SetImage("Under_Water.bmp");
-		WaveBack->SetPosition(WaveBackPos);
-		WaveBack->SetScale(BackScale);
-	}
-	//Wave Animation
+    //BackGround_Sky
+    {
+        GameEngineRender* BackGround = CreateRender(WormsRenderOrder::BackGround);
+        BackGround->SetImage("gradient.bmp");
+        BackGround->SetPosition(MapScale.half());
+        BackGround->SetScaleToImage();
+    }
+    //BackGround_Mountain
+    {
+        GameEngineRender* BackGround = CreateRender(WormsRenderOrder::BackGround);
+        BackGround->SetImage("Midground.bmp");
+        BackGround->SetPosition(MountainPos);
+        BackGround->SetScale(BackScale);
+    }
+    //BackGround_Wave
+    {
+        GameEngineRender* WaveBack = CreateRender(WormsRenderOrder::BackGround);
+        WaveBack->SetImage("Under_Water.bmp");
+        WaveBack->SetPosition(WaveBackPos);
+        WaveBack->SetScale(BackScale);
+    }
+    //Wave Animation
     {
         GameEngineRender* Wave0 = CreateRender(WormsRenderOrder::BackGround);
         Wave0->SetImage("Water_sprite.bmp");
@@ -115,53 +115,62 @@ void Map::Start()
         Wave0->ChangeAnimation("Wave0");
 
     }
-	int RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);					//Animation을 시작할 랜덤한 인덱스
-	{
-		GameEngineRender* Wave1 = CreateRender(WormsRenderOrder::Wave);
-		Wave1->SetImage("Water_sprite.bmp");
-		Wave1->SetPosition(WaveAnimPos1);
-		Wave1->SetScale(WaveScale);
-		Wave1->CreateAnimation({ .AnimationName = "Wave1",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
-		Wave1->ChangeAnimation("Wave1", RandIdx);
+    int RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);					//Animation을 시작할 랜덤한 인덱스
+    {
+        GameEngineRender* Wave1 = CreateRender(WormsRenderOrder::Wave);
+        Wave1->SetImage("Water_sprite.bmp");
+        Wave1->SetPosition(WaveAnimPos1);
+        Wave1->SetScale(WaveScale);
+        Wave1->CreateAnimation({ .AnimationName = "Wave1",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
+        Wave1->ChangeAnimation("Wave1", RandIdx);
 
-	}
+    }
     RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);
-	{
-		GameEngineRender* Wave2 = CreateRender(WormsRenderOrder::Wave);
-		Wave2->SetImage("Water_sprite.bmp");
-		Wave2->SetPosition(WaveAnimPos2);
-		Wave2->SetScale(WaveScale);
-		Wave2->CreateAnimation({ .AnimationName = "Wave2",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
-		Wave2->ChangeAnimation("Wave2", RandIdx);
-	}
-	RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);
-	{
-		GameEngineRender* Wave3 = CreateRender(WormsRenderOrder::Wave);
-		Wave3->SetImage("Water_sprite.bmp");
-		Wave3->SetPosition(WaveAnimPos3);
-		Wave3->SetScale(WaveScale);
-		Wave3->CreateAnimation({ .AnimationName = "Wave3",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
-		Wave3->ChangeAnimation("Wave3", RandIdx);
+    {
+        GameEngineRender* Wave2 = CreateRender(WormsRenderOrder::Wave);
+        Wave2->SetImage("Water_sprite.bmp");
+        Wave2->SetPosition(WaveAnimPos2);
+        Wave2->SetScale(WaveScale);
+        Wave2->CreateAnimation({ .AnimationName = "Wave2",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
+        Wave2->ChangeAnimation("Wave2", RandIdx);
+    }
+    RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);
+    {
+        GameEngineRender* Wave3 = CreateRender(WormsRenderOrder::Wave);
+        Wave3->SetImage("Water_sprite.bmp");
+        Wave3->SetPosition(WaveAnimPos3);
+        Wave3->SetScale(WaveScale);
+        Wave3->CreateAnimation({ .AnimationName = "Wave3",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
+        Wave3->ChangeAnimation("Wave3", RandIdx);
 
-	}
-	RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);
-	{
-		GameEngineRender* Wave4 = CreateRender(WormsRenderOrder::Wave);
-		Wave4->SetImage("Water_sprite.bmp");
-		Wave4->SetPosition(WaveAnimPos4);
-		Wave4->SetScale(WaveScale);
-		Wave4->CreateAnimation({ .AnimationName = "Wave4",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
-		Wave4->ChangeAnimation("Wave4", RandIdx);
+    }
+    RandIdx = GameEngineRandom::MainRandom.RandomInt(0, 10);
+    {
+        GameEngineRender* Wave4 = CreateRender(WormsRenderOrder::Wave);
+        Wave4->SetImage("Water_sprite.bmp");
+        Wave4->SetPosition(WaveAnimPos4);
+        Wave4->SetScale(WaveScale);
+        Wave4->CreateAnimation({ .AnimationName = "Wave4",  .ImageName = "Water_sprite.bmp", .Start = 0, .End = 10 });
+        Wave4->ChangeAnimation("Wave4", RandIdx);
 
-	}
-    
+    }
 
-    MapObject* Object1 = GetLevel()->CreateActor<MapObject>(WormsRenderOrder::MapObject);
-    float4 pos = Object1->GetRandomPos();
-    Object1->SetPos(pos);
-
-    MapObject* Object2 = GetLevel()->CreateActor<MapObject>(WormsRenderOrder::MapObject);
-    Object2->SetPos(Object2->GetRandomPos());
+    std::string LevelName = "Play";
+    //PlayLevel일 때만, 맵 오브젝트를 생성해서 맵에 적용
+    //TestLevel에서도 허용하게 되면, 맵 start가 2번 돌면서
+    //TestLevel에서 생성한 오브젝트와 PlayLevel에서 생성한 오브젝트들이 
+    //모두 하나의 맵에 적용됨(Map이 static)
+    if (LevelName == GetLevel()->GetName())
+    {
+        int NumOfObj = GameEngineRandom::MainRandom.RandomInt(1, 8);
+        for (int i = 0; i < NumOfObj; i++)
+        {
+            MapObject* Object = GetLevel()->CreateActor<MapObject>(WormsRenderOrder::MapObject);
+            float4 pos = Object->GetRandomPos();
+            Object->SetPos(pos);
+            Object->MergeMap();
+        }
+    }
 }
 
 void Map::Update(float _DeltaTime)
@@ -171,6 +180,7 @@ void Map::Update(float _DeltaTime)
 	{
 		float4 Pos = GetLevel()->GetMousePosToCamera();
 		MapModifier::MainModifier->CreateHole(Pos, 20);
+        //MapModifier::MainModifier->CreateRect(Pos, 10, 6);
 
 		return;
 	}
@@ -250,4 +260,10 @@ void Map::Render(float _DeltaTime)
 	std::string MousePosStr = "MousePosition : ";
 	MousePosStr += GetLevel()->GetMousePosToCamera().ToString();
 	GameEngineLevel::DebugTextPush(MousePosStr);
+
+    std::string NumOfMapObj = "NumOfMapObj : ";
+    /*char Arr[] = NumOfObj;
+
+    NumOfMapObj += NumOfObj
+    GameEngineLevel::DebugTextPush(NumOfMapObj);*/
 }
--- GameEngineContents/MapModifier.cpp
+++ GameEngineContents/MapModifier.cpp
@@ -109,6 +109,81 @@ void MapModifier::CreateHole(float4 _Pos, int _Radius)
 	DrawPixel(_Pos, _Radius);
 }
 
+void MapModifier::CreateRect(float4 _Pos, int _Horz, int _Vert)
+{
+    ModifierCollision->SetPosition(_Pos);
+
+    if (this == nullptr)
+    {
+        MsgAssert("ModifierActor가 nullptr 입니다.");
+        return;
+    }
+
+    if (0 >= _Horz)
+    {
+        MsgAssert("사각형 가로 길이가 0보다 작거나 같을 수 없습니다.");
+        return;
+    }
+
+    if (0 >= _Vert)
+    {
+        MsgAssert("사각형 세로 길이가 0보다 작거나 같을 수 없습니다.");
+        return;
+    }
+
+    HDC MapDc = Map::MainMap->GetMapDC();
+    HDC ColMapDc = Map::MainMap->GetColMapDC();
+
+    float4 RectRenderPos = _Pos;
+
+    //Map에 그림
+    {
+        //선에 대한 팬 생성
+        //PS_SOLID: 실선              || 두께  || 색상
+        HPEN MyPen = CreatePen(PS_SOLID, 1, Magenta);
+        HPEN OldPen = (HPEN)SelectObject(MapDc, MyPen);
+
+        HBRUSH MyBrush = (HBRUSH)CreateSolidBrush(Magenta);                     //단색에 대한 논리적 브러쉬를 생성
+        HBRUSH OldBrush = (HBRUSH)SelectObject(MapDc, MyBrush);                 //지정된 DC로 개체 선택
+
+        Rectangle(MapDc,
+            RectRenderPos.ix() - _Horz,                                         //Left
+            RectRenderPos.iy() - _Vert,                                         //Top
+            RectRenderPos.ix() + _Horz,
+            RectRenderPos.iy() + _Vert);
+
+        SelectObject(MapDc, OldPen);
+        DeleteObject(MyPen);
+
+        SelectObject(MapDc, OldBrush);                                          //다시 기존 브러쉬 선택
+        DeleteObject(MyBrush);                                                  //생성한 브러쉬 삭제
+
+    }
+
+    //ColMap에 그림
+    {
+        HPEN MyPen = CreatePen(PS_SOLID, 1, Magenta);
+        HPEN OldPen = (HPEN)SelectObject(ColMapDc, MyPen);
+
+        HBRUSH MyBrush = (HBRUSH)CreateSolidBrush(Magenta);                     //단색에 대한 논리적 브러쉬를 생성
+        HBRUSH OldBrush = (HBRUSH)SelectObject(ColMapDc, MyBrush);              //지정된 DC로 개체 선택
+
+        Rectangle(ColMapDc,
+            RectRenderPos.ix() - _Horz,
+            RectRenderPos.iy() - _Vert,
+            RectRenderPos.ix() + _Horz,
+            RectRenderPos.iy() + _Vert);
+
+        SelectObject(ColMapDc, OldBrush);
+        DeleteObject(MyBrush);
+
+        SelectObject(ColMapDc, OldPen);
+        DeleteObject(MyPen);
+    }
+
+    //사각형 부분에 구분선 그리는 함수 추가 해야 함
+}
+
 void MapModifier::DrawPixel(float4 _Pos, int _Radius)
 {
 	HDC MapDc = Map::MainMap->GetMapDC();
@@ -122,22 +197,71 @@ void MapModifier::DrawPixel(float4 _Pos, int _Radius)
 
 	float4 CenterPos = _Pos;						                                        //체크 시작점: 원의 중점
 
-	for (; Angle < 370.0f; ++Angle)
+    //360도로 변경해야 함
+	for (; Angle < 90.0f; ++Angle)
 	{
 		float4 CheckPos = { 0.0f, -Radius };		                                        //원점에서 반지름만큼 올라간 점, 각도가 변하기 전 원래 값으로 초기화
 		CheckPos.RotaitonZDeg(Angle);				                                        //회전변환 적용
 		CheckPos += CenterPos;						                                        //회전시킨 후 위치 변화
 
 
-		////충돌맵의 파란색 부분과 깎이는 부분의 원 둘레가 닿으면 색칠
-  //      DWORD color = ColImage->GetPixelColor(CheckPos, Magenta);
+        float4 CheckPos1 = {Radius,  0.0f };		                                        //90에서 시작                                      
+        CheckPos1.RotaitonZDeg(Angle);				
+        CheckPos1 += CenterPos;
 
-  //      //문제점 262 ~279도->Magenta로 확인
-  //      int R = GetRValue(color);
-  //      int G = GetGValue(color);
-  //      int B = GetBValue(color);
+        float4 CheckPos2 = { 0.0f, Radius };                                               //180도에서 시작
+        CheckPos2.RotaitonZDeg(Angle);
+        CheckPos2 += CenterPos;
 
-  //      int a = 10;
+        float4 CheckPos3 = { -Radius, 0.0f };                                               //270도에서 시작
+        CheckPos3.RotaitonZDeg(Angle);
+        CheckPos3 += CenterPos;
+
+
+      //  {
+		    //////충돌맵의 파란색 부분과 깎이는 부분의 원 둘레가 닿으면 색칠
+      //      DWORD color = ColImage->GetPixelColor(CheckPos, Magenta);
+
+      //      //문제점 262 ~279도->Magenta로 확인
+      //      int R = GetRValue(color);
+      //      int G = GetGValue(color);
+      //      int B = GetBValue(color);
+
+      //      int a = 10;
+      //  }
+
+      //  {
+      //      DWORD color = ColImage->GetPixelColor(CheckPos1, Magenta);
+
+      //      //문제점 262 ~279도->Magenta로 확인
+      //      int R = GetRValue(color);
+      //      int G = GetGValue(color);
+      //      int B = GetBValue(color);
+
+      //      int a = 10;
+      //  }
+
+      //  {
+      //      DWORD color = ColImage->GetPixelColor(CheckPos2, Magenta);
+
+      //      //문제점 262 ~279도->Magenta로 확인
+      //      int R = GetRValue(color);
+      //      int G = GetGValue(color);
+      //      int B = GetBValue(color);
+
+      //      int a = 10;
+      //  }
+      //  {
+      //      DWORD color = ColImage->GetPixelColor(CheckPos3, Magenta);
+
+      //      //문제점 262 ~279도->Magenta로 확인
+      //      int R = GetRValue(color);
+      //      int G = GetGValue(color);
+      //      int B = GetBValue(color);
+
+      //      int a = 10;
+      //  }
+
 
 		if (Blue == ColImage->GetPixelColor(CheckPos, Magenta))
 		{
@@ -165,6 +289,90 @@ void MapModifier::DrawPixel(float4 _Pos, int _Radius)
 				DeleteObject(MyPen);
 			}		
 		}
+
+        if (Blue == ColImage->GetPixelColor(CheckPos1, Magenta))
+        {
+            //Map에 그림
+            {
+                //선에 대한 팬 생성
+                //PS_SOLID: 실선
+                HPEN MyPen = CreatePen(PS_SOLID, lineThick, LineColor);
+                HPEN OldPen = (HPEN)SelectObject(MapDc, MyPen);
+
+                HBRUSH MyBrush = (HBRUSH)CreateSolidBrush(LineColor);                       //단색에 대한 논리적 브러쉬를 생성
+                HBRUSH OldBrush = (HBRUSH)SelectObject(MapDc, MyBrush);                     //지정된 DC로 개체 선택
+
+                Ellipse(MapDc,
+                    CheckPos1.ix() - lineThick,
+                    CheckPos1.iy() - lineThick,
+                    CheckPos1.ix() + lineThick,
+                    CheckPos1.iy() + lineThick);
+
+
+                SelectObject(MapDc, OldBrush);                                              //다시 기존 브러쉬 선택
+                DeleteObject(MyBrush);                                                      //생성한 브러쉬 삭제
+
+                SelectObject(MapDc, OldPen);
+                DeleteObject(MyPen);
+            }
+        }
+
+        if (Blue == ColImage->GetPixelColor(CheckPos2, Magenta))
+        {
+            //Map에 그림
+            {
+                //선에 대한 팬 생성
+                //PS_SOLID: 실선
+                HPEN MyPen = CreatePen(PS_SOLID, lineThick, LineColor);
+                HPEN OldPen = (HPEN)SelectObject(MapDc, MyPen);
+
+                HBRUSH MyBrush = (HBRUSH)CreateSolidBrush(LineColor);                       //단색에 대한 논리적 브러쉬를 생성
+                HBRUSH OldBrush = (HBRUSH)SelectObject(MapDc, MyBrush);                     //지정된 DC로 개체 선택
+
+                Ellipse(MapDc,
+                    CheckPos2.ix() - lineThick,
+                    CheckPos2.iy() - lineThick,
+                    CheckPos2.ix() + lineThick,
+                    CheckPos2.iy() + lineThick);
+
+
+                SelectObject(MapDc, OldBrush);                                              //다시 기존 브러쉬 선택
+                DeleteObject(MyBrush);                                                      //생성한 브러쉬 삭제
+
+                SelectObject(MapDc, OldPen);
+                DeleteObject(MyPen);
+            }
+        }
+
+
+        if (Blue == ColImage->GetPixelColor(CheckPos3, Magenta))
+        {
+            //Map에 그림
+            {
+                //선에 대한 팬 생성
+                //PS_SOLID: 실선
+                HPEN MyPen = CreatePen(PS_SOLID, lineThick, LineColor);
+                HPEN OldPen = (HPEN)SelectObject(MapDc, MyPen);
+
+                HBRUSH MyBrush = (HBRUSH)CreateSolidBrush(LineColor);                       //단색에 대한 논리적 브러쉬를 생성
+                HBRUSH OldBrush = (HBRUSH)SelectObject(MapDc, MyBrush);                     //지정된 DC로 개체 선택
+
+                Ellipse(MapDc,
+                    CheckPos3.ix() - lineThick,
+                    CheckPos3.iy() - lineThick,
+                    CheckPos3.ix() + lineThick,
+                    CheckPos3.iy() + lineThick);
+
+
+                SelectObject(MapDc, OldBrush);                                              //다시 기존 브러쉬 선택
+                DeleteObject(MyBrush);                                                      //생성한 브러쉬 삭제
+
+                SelectObject(MapDc, OldPen);
+                DeleteObject(MyPen);
+            }
+        }
+
+
         /*else
         {
             Rectangle(MapDc,
